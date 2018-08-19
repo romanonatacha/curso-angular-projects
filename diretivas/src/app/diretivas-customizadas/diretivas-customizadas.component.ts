f8150d74a473ffeea6911caadd15b6454db8d428
Natacha Romano <romano.n@hotmail.com>
2018-08-19 17:22:08 -0300

    #36: Criando uma diretiva de estrutura (ngElse)

--- diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
+++ diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivasCustomizadasComponent implements OnInit {
 
+  mostrarCursos: boolean = false;
+
   constructor() { }
+
+  onMostrarCursos() {
+    this.mostrarCursos = !this.mostrarCursos;
+  }
 
   ngOnInit() {
   }
