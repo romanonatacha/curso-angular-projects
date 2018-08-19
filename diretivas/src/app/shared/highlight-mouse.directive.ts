395485176ea4bc5bc616b558696b6c3c0cb53caf
Natacha Romano <romano.n@hotmail.com>
2018-08-19 16:42:54 -0300

    #34: Diretivas: HostListener e HostBinding

---
+++ diretivas/src/app/shared/highlight-mouse.directive.ts
@@ -0,0 +1,21 @@
+import { element } from 'protractor';
+import { Directive, HostListener, ElementRef, Renderer2, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[highlightMouse]'
+})
+export class HighlightMouseDirective {
+
+  @HostListener('mouseenter') onMouseOver() {
+    this.backgroundColor = 'yellow';
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.backgroundColor = 'white';
+  }
+
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
+  constructor() { }
+
+}
