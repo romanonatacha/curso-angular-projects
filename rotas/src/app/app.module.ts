816e8facaa3b03b662d0da2e47166815fbf2d062
Natacha Romano <romano.n@hotmail.com>
2018-09-08 19:27:02 -0300

    53: Rotas: Definindo e extraindo parâmetros de roteamento

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
-import { routing } from './app.routing'
+import { routing } from './app.routing';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
