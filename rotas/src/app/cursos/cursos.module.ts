44fe6f853bcd7e9eaba2fa616bdc3502acb70bba
Natacha Romano <romano.n@hotmail.com>
2018-09-09 19:05:46 -0300

    #59: Rotas: Criando um módulo de rotas de funcionalidade

--- rotas/src/app/cursos/cursos.module.ts
+++ rotas/src/app/cursos/cursos.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+// import { RouterModule } from '@angular/router';
 
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursosRoutingModule } from './cursos.routing.module';
 
 import { CursosService } from './cursos.service';
 
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule
+        CursosRoutingModule
+        // RouterModule
     ],
     exports: [],
     declarations: [
